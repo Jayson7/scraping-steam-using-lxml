5540544e0210197c8acbe754dcf5825e1745e3e5
jayson7 <lexxiijoo70@gmail.com>
2022-02-10 14:23:21 +0100

    scrape steam

--- scraping steam using lxml/main.py
+++ scraping steam using lxml/main.py
@@ -1,9 +1,43 @@
 import requests 
 import lxml.html
+# get the url needed
 html = requests.get('https://store.steampowered.com/explore/new/')
+# pass in  the response
 doc = lxml.html.fromstring(html.content)
+# fetch for new release using the tag associated which  is the div tag in the case, take the first one of index 0
 new_releases = doc.xpath('//div[@id="tab_newreleases_content"]')[0]
 # print(str(new_releases))
+# extract the titles of the results 
 titles = new_releases.xpath('.//div[@class="tab_item_name"]/text()')
 # print(titles)
+#  prices too
 prices = new_releases.xpath('.//div[@class="discount_final_price"]/text()')
+
+tags = []
+for tag in new_releases.xpath('.//div[@class="tab_item_top_tags"]'):
+    tags.append(tag.text_content())
+tags = [tag.split(', ') for tag in tags]
+platforms_div = new_releases.xpath('.//div[@class="tab_item_details"]')
+total_platforms = []
+
+
+for game in platforms_div:
+    temp = game.xpath('.//span[contains(@class, "platform_img")]')
+    platforms = [t.get('class').split(' ')[-1] for t in temp]
+    if 'hmd_separator' in platforms:
+        platforms.remove('hmd_separator')
+
+    total_platforms.append(platforms)
+
+
+output = []
+for info in zip(titles,prices, tags, total_platforms):
+    resp = {}
+    resp['title'] = info[0]
+    resp['price'] = info[1]
+    resp['tags'] = info[]
+    resp['platforms'] = info[3]
+    output.append(resp)
+
+
+print(output)
