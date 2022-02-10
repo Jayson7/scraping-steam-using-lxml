d502f93dba2619dd6ef8a9f53ef79ab9575a24a0
jayson7 <lexxiijoo70@gmail.com>
2022-02-10 13:42:12 +0100

    scrape steam

--- scraping steam using lxml/main.py
+++ scraping steam using lxml/main.py
@@ -2,3 +2,7 @@ import requests
 import lxml.html
 html = requests.get('https://store.steampowered.com/explore/new/')
 doc = lxml.html.fromstring(html.content)
+new_releases = doc.xpath('//div[@id="tab_newreleases_content"]')[0]
+# print(str(new_releases))
+titles = new_releases.xpath('.//div[@class="tab_item_name"]/text()')
+print(titles)
